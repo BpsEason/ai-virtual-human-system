2ac0bddf46d67f80d801ac95b1c239994f7c6cbf
ChenLab <k4917552@gmail.com>
2025-08-03 15:25:33 +0800

    Delete frontend/src directory

--- frontend/src/views/Dashboard.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-6">
-    <h1 class="text-3xl font-bold mb-6">儀表板</h1>
-    <p>這裡是儀表板，顯示系統概覽。</p>
-  </div>
-</template>
--- frontend/src/views/KnowledgeBase.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-6">
-    <h1 class="text-3xl font-bold mb-6">知識庫管理</h1>
-    <p>這裡是管理知識庫文件的頁面。</p>
-  </div>
-</template>
